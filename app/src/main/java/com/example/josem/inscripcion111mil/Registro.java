74dd2b96ee044888bfe65bc88f4d4012e7347a31
Chaky <josemonzon99@gmail.com>
2018-12-27 15:36:29 -0300

    Se agrego lista de provincias

--- app/src/main/java/com/example/josem/inscripcion111mil/Registro.java
+++ app/src/main/java/com/example/josem/inscripcion111mil/Registro.java
@@ -3,8 +3,12 @@ package com.example.josem.inscripcion111mil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -14,6 +18,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,10 +26,39 @@ public class Registro extends AppCompatActivity {
     EditText nombre, apellido, email;
     Button inscripcion;
     RequestQueue requestQueue;
+    private Spinner escuela;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        escuela = (Spinner)findViewById(R.id.escuela);
+
+        ArrayList<String> escuelas = new ArrayList<>();
+
+        escuelas.add("Buenos Aires");
+        escuelas.add("Santa Fe");
+        escuelas.add("Mendoza");
+
+
+        ArrayAdapter adp = new ArrayAdapter(Registro.this, android.R.layout.simple_spinner_dropdown_item, escuelas);
+
+        escuela.setAdapter(adp);
+        escuela.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String elemento = (String) escuela.getAdapter().getItem(position);
+
+                Toast.makeText(Registro.this,"Seleccionaste: " + elemento, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         nombre = (EditText)findViewById(R.id.nombre);
         apellido = (EditText)findViewById(R.id.apellido);
         email = (EditText)findViewById(R.id.email);
